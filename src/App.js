841174954558030b4eb5463293feb50d4652e585
Priyosmita <priyosh08@gmail.com>
2023-01-27 19:48:20 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
+import { RiDeleteBinLine } from "react-icons/ri";
 import "./App.css";
+import { BsCheck2Circle } from "react-icons/bs";
 
 function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState(() => {
     return JSON.parse(localStorage.getItem("list")) || [];
   });
+
+  const inputRef = useRef(null);
 
   useEffect(() => {
     localStorage.setItem("list", JSON.stringify(list));
@@ -17,9 +22,15 @@ function App() {
   };
 
   const handleSubmit = () => {
-    const newArr = [input, ...list];
+    if (!input.trim()) return;
+    const entry = {
+      text: input,
+      status: "todo",
+    };
+    const newArr = [entry, ...list];
     setList(newArr);
     setInput("");
+    inputRef.current.focus();
   };
 
   const handleDelete = (index) => {
@@ -32,26 +43,37 @@ function App() {
     let newArr = [...list];
     const workdone = newArr[index];
     newArr.splice(index, 1);
+    workdone.status = "complete";
     newArr = [...newArr, workdone];
     setList(newArr);
   };
 
   return (
     <div className="App">
-      <div>
-        <h1> ToDo List</h1>
+      <h1 className="headline">Tasks</h1>
+      <div className="inputWorkdiv">
+        <input
+          ref={inputRef}
+          type="text"
+          className="inputBar"
+          value={input}
+          onChange={handleInput}
+        />
+
+        <button className="addButton" onClick={handleSubmit}>
+          Add
+        </button>
       </div>
-      <div>
-        <input type="text" className="" value={input} onChange={handleInput} />
-        <button onClick={handleSubmit}>Add</button>
-      </div>
-      <div>
+
+      <div className="listContainer">
         {list.map((item, index) => {
           return (
-            <div key={index}>
-              <p>{item}</p>
-              <button onClick={() => handleComplete(index)}>Complete</button>
-              <button onClick={() => handleDelete(index)}>Delete</button>
+            <div key={index} className="listItem">
+              <p className={`textbox ${item.status==="complete" && "strike"}`}>{item.text}</p>
+              <div className="taskButtons">
+                <BsCheck2Circle className={item.status==="complete" && "completed"} onClick={() => handleComplete(index)} />
+                <RiDeleteBinLine onClick={() => handleDelete(index)} />
+              </div>
             </div>
           );
         })}
